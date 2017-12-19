aa4aa11c787d53d1c952db07fae97b510000b779
Itamar Hassin <ihassin@mac.com>
2017-12-19 11:32:40 -0500

    Added ;

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 class App extends Component {
   render() {
-    document.title = "Ita"
+    document.title = "Ita";
     return (
       <div className="App">
         <header className="App-header">
